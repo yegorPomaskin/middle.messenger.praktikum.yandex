8a6ba8efd9ae9e3cf0dd4a1f661b14d252731fb4
Yegor.Twin <yegor.pomaskin@gmail.com>
2025-03-09 23:17:53 +0300

    vite and handlebars added

---
+++ types/custom.d.ts
@@ -0,0 +1,9 @@
+declare module "*.css" {
+  const css: { [key: string]: string };
+  export default css;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  root: resolve(__dirname, "static"), // Root is the 'static' folder
+  build: {
+    outDir: "../dist", // Output directory
+  },
+  resolve: {
+    alias: {
+      "/src": resolve(__dirname, "src"),
+    },
+  },
+  publicDir: resolve(__dirname, "public"),
+  plugins: [
+    handlebars({
+      context: {
+        title: "Hello, world!",
+      },
+      partialDirectory: resolve(__dirname, "src/partials"),
+    }),
+  ],
+});
