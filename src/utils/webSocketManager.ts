ec0fb32c8c5ec6696284649ae8231ff186d23060
Yegor.Twin <yegor.pomaskin@gmail.com>
2025-05-29 01:56:37 +0300

    finished for review

--- src/utils/webSocketManager.ts
+++ src/utils/webSocketManager.ts
@@ -32,7 +32,7 @@ class WebSocketManager {
       onMessage?: MessageHandler;
       onHistory?: HistoryHandler;
       onUserConnected?: UserConnectHandler;
-    },
+    }
   ): Promise<void> {
     const user = AuthController.getUserData();
     this.userId = user?.id || null;
@@ -83,7 +83,7 @@ class WebSocketManager {
       onMessage?: MessageHandler;
       onHistory?: HistoryHandler;
       onUserConnected?: UserConnectHandler;
-    },
+    }
   ): Promise<void> {
     const { token } = await ChatAPI.getChatToken(chatId);
     const user = AuthController.getUserData();
@@ -151,6 +151,10 @@ class WebSocketManager {
 
   public isConnected(): boolean {
     return this.socket?.readyState === WebSocket.OPEN;
+  }
+
+  public getCurrentChatId(): number | null {
+    return this.chatId;
   }
 }
 
